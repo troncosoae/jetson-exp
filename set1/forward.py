74fc8bc5ef9291762498267334b5b02307bdba73
atroncoso2 <atroncoso2@uc.cl>
2021-09-03 12:00:04 -0400

    forward tracking execution time

--- set1/forward.py
+++ set1/forward.py
@@ -6,6 +6,7 @@ from data_worker import import_data
 from Net.Net import Net
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 # functions to show an image
 
@@ -19,14 +20,18 @@ def imshow(img):
 
 if __name__ == '__main__':
     WEIGHTS_PATH = sys.argv[1]
-    print(f'forward.py {WEIGHTS_PATH}')
+    DEVICE = sys.argv[2]
+    print(f'forward.py {WEIGHTS_PATH} {DEVICE}')
 
     # verifying cuda available
     print('checking cuda availability...')
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device('cpu')
+    if DEVICE == 'cuda':
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f'device: {device}')
 
     # importing data
+    start_time = time.time()
     print('importing data...')
     transform = transforms.Compose(
         [transforms.ToTensor(),
@@ -38,12 +43,15 @@ if __name__ == '__main__':
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
     trainloader, testloader = import_data('./data', batch_size, transform)
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     # get net and import weights
+    start_time = time.time()
     print('loading network...')
     net = Net()
     net.to(device)
     net.load_state_dict(torch.load(WEIGHTS_PATH))
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     # get batch of images
     print('get test images...')
@@ -55,11 +63,13 @@ if __name__ == '__main__':
     print('GroundTruth: ', ' '.join(
         '%5s' % classes[labels[j]] for j in range(4)))
 
-    images, labels = images.to(device),labels.to(device)
+    images, labels = images.to(device), labels.to(device)
 
     # running forward
     print('running forward...')
+    start_time = time.time()
     outputs = net(images)
+    print("--- %s seconds ---" % (time.time() - start_time))
     _, predicted = torch.max(outputs, 1)
 
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
